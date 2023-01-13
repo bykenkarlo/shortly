19ce8730822094c424a88283e57916bfda609f30
blankcodes <me@kenkarlo.com>
2023-01-13 11:26:52 +0800

    Update terms of use

--- application/views/pages/terms.php
+++ application/views/pages/terms.php
@@ -8,14 +8,23 @@
                         <div class="col-lg-12">
                             <div class="card card-services-highlights pt-2 pb-2 padding-right-30 padding-left-30">
                                 <div class="mt-3 mb- ">
-                                    <h1 class="c-gray font-25">Terms and Conditions</h1>
-                                    <small class="c-gray">Last Revised: Jan. 4, 2023</small>
+                                    <h1 class="c-gray font-25">Terms of Use</h1>
+                                    <small class="c-gray">Last Revised: Jan. 12, 2023</small>
                                 </div>
                                 <div class="mt-2">
                                     <p class="p-text">
                                         By using the Shortly website, you consent to the data practices described in this statement.
                                     </p>
-                                    <h2 class="font-20 c-gray mt-3">Collection of your Information</h2>
+
+                                    <p class="p-text">
+                                    The User is obligated to use the Website in a legitimate way and in accordance with good manners. The User is not permitted to provide any illegitimate content. 
+                                    We have the right to remove your link once reported to be suspicious, malicious/spam, violence, etc.
+                                    </p>
+                                    <h2 class="font-20 c-gray mt-3 fw-700">Privacy Policy</h2>
+                                    <p class="p-text">
+                                    Through Your Use of the Website and Services, You may provide Us with certain information. 
+                                    </p>
+                                    <h2 class="font-20 c-gray mt-3 fw-700">Collection of your Information</h2>
                                     <p class="p-text">
                                     In order to better provide you with features offered, Shortly may collect information, such as your:
                                     <ul class="p-text">
@@ -26,18 +35,18 @@
                                         <li>Location - City</li>
                                     </ul>
                                     </p>
-                                    <h2 class="font-20 c-gray mt-3">IP Address</h2>
+                                    <h2 class="font-20 c-gray mt-3 fw-700">IP Address</h2>
                                     <p class="p-text">
                                         Shortly use IP Geolocation API such as IPinfo.io to identify the Users' Country and City to track Users' location who click the shortened URL. This is to provide accurate monitoring statistics of each clicks. We don't save your IP Address on our database.
                                     </p>
 
-                                    <h2 class="font-20 c-gray mt-3">Cookies</h2>
+                                    <h2 class="font-20 c-gray mt-3 fw-700">Cookies</h2>
                                     <p class="p-text">
                                         We use cookies to record your preferences to our site, and for certain site functions. Cookie information is never passed to our database. 
                                         We uses cookies to store information which helps enhance the responsiveness and customizability for the graphical interface of the site. 
                                     </p>
 
-                                    <h2 class="font-20 c-gray mt-3">Google Analytics</h2>
+                                    <h2 class="font-20 c-gray mt-3 fw-700">Google Analytics</h2>
                                     <p class="p-text">
                                         We use service providers, such as Google Analytics, which use cookies and other technologies to collect information about your traffic across this site.
                                         To learn more about how Google Analytics collects and processes data and the choices Google may offer to control these activities, check <a rel="nofollow" target="_blank" href="https://support.google.com/analytics/answer/6004245">https://support.google.com/analytics/answer/6004245</a>. 
